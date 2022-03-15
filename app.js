f2ce915f6c334c8fc7a3da10fe0c669d9607e776
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-03-15 23:51:31 +0700

    learn flex

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'rubicamp',
+  secret: 'eja',
   resave: true,
   saveUninitialized: true
 }))
